c810225d55048c817274f97152bf8115033d82f3
jackmford <jackmitchellfordyce@gmail.com>
2025-02-19 16:44:32 -0600

    fix: remove on message since we use on clicked now

--- background.js
+++ background.js
@@ -1,21 +1,3 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === 'SAVE_HIGHLIGHT') {
-    chrome.storage.sync.get('highlights', (data) => {
-      let highlights = data.highlights || {}
-      let highlightMap = highlights[message.url] || []
-
-      highlightMap.push({ text: message.text, timestamp: Date.now() })
-
-      highlights[message.url] = highlightMap
-      //highlights.push({ text: message.text, url: message.url, timestamp: Date.now() });
-
-      chrome.storage.sync.set({ highlights }, () => {
-        console.log('Highlight saved:', message.text)
-      })
-    })
-  }
-})
-
 chrome.runtime.onInstalled.addListener(() => {
   // Create a custom context menu item for selected text
   chrome.contextMenus.create({
